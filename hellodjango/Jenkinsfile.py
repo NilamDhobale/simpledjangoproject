4b1c8f7aef26ad6d30275e1f61495b53babeced4
NilamDhobale <nilamdhobale11@gmail.com>
2021-10-20 12:22:29 +0530

    Update Jenkinsfile.py

--- hellodjango/Jenkinsfile.py
+++ hellodjango/Jenkinsfile.py
@@ -44,7 +44,7 @@ pipeline {
         stage('Docker Run') {
             steps{
                 script {
-                    dockerImage.run("-p 3000:3000 --rm --name pythonappContainer")
+                    dockerImage.run("-p 8000:8000 --rm --name pythonappContainer")
                 }
             }
         }
